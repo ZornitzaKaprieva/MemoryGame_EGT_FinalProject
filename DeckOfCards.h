5faf6e4fa0dd758a0f4ffa1c9feeb7f1f7d4e5ce
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2024-02-20 18:50:38 +0200

    MemoryGame_EGT_FinalProject

--- DeckOfCards.h
+++ DeckOfCards.h
@@ -1,17 +1,17 @@
-//// Card structure definition
-//#include <string>
-//#include"GameObject.h";
-//
-//class DeckOfCards
-//{
-//public:
-//	DeckOfCards(); // constructor initializes deck
-//	void shuffle(); // shuffles cards in deck
-//	//void deal(); // deals cards
-// 
-//private:
-//	//Card deck16[16]; // represents deck of 16 cards
-//	//Card deck32[32]; // represents deck of 32 cards 
-//	//Card deck64[64]; // represents deck of 64 cards 
-//
-//};
+
+#include <string>
+#include"GameObject.h";
+
+class DeckOfCards
+{
+public:
+	DeckOfCards(); // constructor initializes deck
+	void shuffle(); // shuffles cards in deck
+	void deal(); // deals shuffled cards
+ 
+private:
+	GameObject* gameObject;
+	static std::vector<GameObject*> gameObjects;
+
+
+};
